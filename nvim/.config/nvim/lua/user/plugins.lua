4061799be7b679d92dc4ddd7dc2852e4f5032de2
Marius Stan <hyperion1209@gmail.com>
2022-06-13 19:57:31 +0200

    Adding whichkey

--- nvim/.config/nvim/lua/user/plugins.lua
+++ nvim/.config/nvim/lua/user/plugins.lua
@@ -63,6 +63,7 @@ return packer.startup(function(use)
   use "hrsh7th/cmp-nvim-lua"
   use "akinsho/bufferline.nvim"
   use "moll/vim-bbye"
+  use "folke/which-key.nvim"
 
   -- snippets
   use "L3MON4D3/LuaSnip" --snippet engine
