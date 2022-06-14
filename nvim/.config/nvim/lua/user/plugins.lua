8ca887562c81eeddbcca9c8a54c61ea8267d925e
Marius Stan <hyperion1209@gmail.com>
2022-06-14 21:24:22 +0200

    Adding indent line

--- nvim/.config/nvim/lua/user/plugins.lua
+++ nvim/.config/nvim/lua/user/plugins.lua
@@ -50,6 +50,7 @@ return packer.startup(function(use)
   use 'kyazdani42/nvim-tree.lua'
   use "akinsho/toggleterm.nvim"
   use "lewis6991/impatient.nvim"
+  use "lukas-reineke/indent-blankline.nvim"
 
   -- Colorschemes
   use "LunarVim/darkplus.nvim"
