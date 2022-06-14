57897c4e24c5e66397bb5ae5e42cf7ad004661d6
Marius Stan <hyperion1209@gmail.com>
2022-06-14 20:33:34 +0200

    Add projects

--- nvim/.config/nvim/lua/user/plugins.lua
+++ nvim/.config/nvim/lua/user/plugins.lua
@@ -66,6 +66,7 @@ return packer.startup(function(use)
   use "moll/vim-bbye"
   use 'nvim-lualine/lualine.nvim'
   use "folke/which-key.nvim"
+  use "ahmedkhalf/project.nvim"
 
   -- snippets
   use "L3MON4D3/LuaSnip" --snippet engine
